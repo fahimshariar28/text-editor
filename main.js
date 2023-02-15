895ae45527cfa2bb835cc8f04faa9da5f7510669
Fahim Shariar <107372111+fahimshariar28@users.noreply.github.com>
2023-02-15 15:34:36 +0600

    Update main.js
    Added Comments and Changed a variable name in Font Size function

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
+// Text Field Variable Declared
 const text = document.getElementById("text");
+
+// Bold Button Function
 document.getElementById("bold-btn").addEventListener("click", function () {
   if (text.style.fontWeight === "bold") {
     text.style.fontWeight = "normal";
@@ -6,6 +9,8 @@ document.getElementById("bold-btn").addEventListener("click", function () {
     text.style.fontWeight = "bold";
   }
 });
+
+// Italic Button Function
 document.getElementById("italic-btn").addEventListener("click", function () {
   if (text.style.fontStyle === "italic") {
     text.style.fontStyle = "normal";
@@ -13,6 +18,8 @@ document.getElementById("italic-btn").addEventListener("click", function () {
     text.style.fontStyle = "italic";
   }
 });
+
+// Underline Button Function
 document.getElementById("underline-btn").addEventListener("click", function () {
   if (text.style.textDecoration === "underline") {
     text.style.textDecoration = "none";
@@ -20,18 +27,28 @@ document.getElementById("underline-btn").addEventListener("click", function () {
     text.style.textDecoration = "underline";
   }
 });
+
+// Align Right Button Function
 document.getElementById("right-btn").addEventListener("click", function () {
   text.style.textAlign = "right";
 });
+
+// Align Center Button Function
 document.getElementById("center-btn").addEventListener("click", function () {
   text.style.textAlign = "center";
 });
+
+// Align Left Button Function
 document.getElementById("left-btn").addEventListener("click", function () {
   text.style.textAlign = "left";
 });
+
+// Align Justify Button Function
 document.getElementById("justify-btn").addEventListener("click", function () {
   text.style.textAlign = "justify";
 });
+
+// Uppercase & Lowercase Button Function
 document.getElementById("case").addEventListener("click", function () {
   if (text.style.textTransform === "uppercase") {
     text.style.textTransform = "lowercase";
@@ -39,11 +56,15 @@ document.getElementById("case").addEventListener("click", function () {
     text.style.textTransform = "uppercase";
   }
 });
+
+// Font Size Button Function
 document.getElementById("font-size").addEventListener("input", function () {
   const fontNumber = document.getElementById("font-size");
-  const value = fontNumber.value;
-  text.style.fontSize = value + "px";
+  const fontValue = fontNumber.value;
+  text.style.fontSize = fontValue + "px";
 });
+
+// Text Color Button Function
 document.getElementById("text-color").addEventListener("input", function () {
   const textColor = document.getElementById("text-color");
   const colorValue = textColor.value;
